520ca5c94b28b154fdbbb924425e534136fe4896
GONNE <gonne411@gmail.com>
2025-08-03 03:37:35 +0200

    made the cards values to be acessed globally, fixed minor issue in compare funtion, added doc strings and lastly few comments

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import random
 from art import logo
 
-
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 def deal_card():
-    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    """gives cards at random to both player and computer"""
     card =random.choice(cards)
     return card
 
 def calculate_score(cards):
+    """It ensures the cards are the required number at the start,
+    adds the given to ensure their 21 and finally works the logic of whether an ACE should be 11 or 1"""
     if sum(cards) == 21 and len(cards) == 2:
         return 0
 
@@ -19,8 +21,9 @@ def calculate_score(cards):
     return sum(cards)
 
 def compare(u_score, c_score):
-    if u_score == c_score:
-        return "draw"
+    """contains teh comparasion logic"""
+    if u_score == 0 and c_score == 0:
+        return "It's a draw, both have BlackJacks!"
     elif c_score == 0:
         return "Lose, Computer has a Blackjack"
     elif u_score == 0:
@@ -35,6 +38,7 @@ def compare(u_score, c_score):
         return "You lose!"
 
 def play_again():
+    """contains main game logic"""
     print(logo)
     user_cards = []
     computer_cards = []
@@ -43,8 +47,8 @@ def play_again():
     game_over = False
 
     for card in range(2):
-        user_cards.append(deal_card())
-        computer_cards.append(deal_card())
+        user_cards.append(deal_card()) #here i attached cards to the user and computer list by calling the deal function
+        computer_cards.append(deal_card()) # then attaching 2 cards, in list we count from 0
 
     while not game_over:
         computer_score = calculate_score(computer_cards)
@@ -61,9 +65,9 @@ def play_again():
             else:
                 game_over = True
 
-    while computer_score != 0 and computer_score > 21:
+    while computer_score != 0 and computer_score <= 17:
         computer_cards.append(deal_card())
-        computer_score = callable(computer_cards)
+        computer_score = calculate_score(computer_cards)
 
     print(f"Your final cards are: {user_cards} & current score: {user_score}")
     print(f"Computer's final cards are: {computer_cards} & current score: {computer_score}")
@@ -76,9 +80,14 @@ while True:
         print("\n" * 25)
         play_again()
     elif play == "n":
+        print("Thanks for playing!")
         break
     else:
         print("Invalid choice, please enter Y/N")
+
+
+if __name__ =="__main.py__":
+     play_again()
 
 
 
